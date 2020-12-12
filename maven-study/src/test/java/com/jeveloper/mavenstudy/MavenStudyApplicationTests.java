79b7f505b72fcb7d95a7a7812e939ea6b9af7084
mrpillai <pillaimridu@gmail.com>
2020-12-12 19:38:20 +0530

    secondt commit

---
+++ maven-study/src/test/java/com/jeveloper/mavenstudy/MavenStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeveloper.mavenstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavenStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
